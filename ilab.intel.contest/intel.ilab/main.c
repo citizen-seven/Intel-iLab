c934633f45c08e1b57b06c275c67c2d4bd7df708
citizenseven <iskhakov@frtk.ru>
2015-10-09 21:29:37 +0300

    Needs CR
    Finally, it’s working!

---
+++ ilab.intel.contest/intel.ilab/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int i,count=0;
+    char brackets[100];
+    scanf("%s",brackets);
+    for (i=0; i<strlen(brackets); i++) {
+        if (brackets[i]=='(') {
+            count=count+1;
+        }
+        else {
+            if (brackets[i]==')') {
+            count=count-1;
+            }
+        }
+        if (count<0) {
+            break;
+        }
+    }
+    if (count==0) {
+        printf("DONE");
+    }
+    else {
+        printf("MISTAKE!");
+    }
+}
