91d1075dd34d1d15b9c8ee22fc08da7164d44bae
citizenseven <iskhakov@frtk.ru>
2015-10-09 22:23:50 +0300

    NO CR
    OLD PROGRAM

--- iLab.Entering.Contest/iLab.Entering.Contest/main.c
+++ iLab.Entering.Contest/iLab.Entering.Contest/main.c
@@ -11,7 +11,7 @@ int main()
         }
         else {
             if (brackets[i]==')') {
-            count=count-1;
+                count=count-1;
             }
         }
         if (count<0) {
