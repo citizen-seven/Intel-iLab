8a466c719f1c6dfe5f2772f9f5706deb76d6ae3f
citizenseven <iskhakov@frtk.ru>
2015-11-29 18:04:56 +0300

    Assembler works for all types of functions.

--- FullStack v2.1/FullStack v2.1/asm.c
+++ FullStack v2.1/FullStack v2.1/asm.c
@@ -2,52 +2,105 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX_LEN 100
+
 /*typedef struct cmdInfo {
  int num;
  const char* name;
  } cmdInfo; */
 
 int main() {
-    int i=0;
+    int com_count=0;
+    int labels[10] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
     char* buff;
     char* str=(char*) calloc(100, sizeof(int));
-    FILE * out;
-    out = fopen("out.txt","w");
-    FILE * in;
-    in = fopen("in.txt","r");
+    FILE* out = fopen("out.txt","w");
+    FILE* in = fopen("in.txt","r");
+    enum argument_type {
+        none = 0, 
+        num = 1,
+        label = 2,
+        regs = 3
+    };
+
     //creating enum
-#define DEF_CMD(name,num,rule,argnum) cmd_##name=num, //##- склеивание
+#define DEF_CMD(name,num,rule,argtype) cmd_##name=num, //##- склеивание
     enum Commands {
 #include "cmdlist.h"
         cmd_Last
     };
 #undef DEF_CMD
-    
-    /*creating struct
-     #define DEF_CMD(name,num,rule,argnum) \
-     {cmd_##name, #name},
-     const cmdInfo Info[] =
-     {
-     #include "cmdlist.h"
-     {}
-     };
-     #undef DEF_CMD
-     */
-    while (1) {
-        fgets(str, 50, in);
+    while (!feof(in)) {
+        fgets(str, MAX_LEN, in);
         printf ("%s\n", str);
-        i++;
+        com_count++;
         buff = (strtok(str, " "));
-#define DEF_CMD(name,num,rule,argnum) \
-if (strcmp(buff, #name) == 0){ \
-fprintf(out, "%d ", cmd_##name);\
-if (argnum>0) { \
-buff = (strtok(NULL, "\n"));\
-fprintf(out, "%s ", buff);}}\
-else
+        if (strcmp(buff, ":label") == 0) {
+            buff = (strtok(NULL, "\n"));
+            if (((int)*buff)-'0'<10) { //doesn't detect wrong size!!!!!!!!
+                labels[(int)*buff-'0'] = ++com_count;
+                com_count--;
+            } else {
+                printf("Label number is over 10\n");
+                exit(-1);
+            }
+        }
+    };
+    
+    rewind(in);
+    while (!feof(in)) {
+        fgets(str, MAX_LEN, in);
+        printf ("%s\n", str);
+        buff = (strtok(str, " "));
+        if (strcmp(buff, ":label") != 0) {
+#define DEF_CMD(name,num,rule,argtype) \
+            if (strcmp(buff, #name) == 0) {\
+                fprintf(out, "%d ", cmd_##name);\
+                switch (argtype) {\
+                    case 1:\
+                        buff = (strtok(NULL, " \n"));\
+                        if (buff != NULL) {\
+                            fprintf(out, "%s ", buff);\
+                        } else {\
+                            printf("Function %d needs an argument\n", cmd_##name);\
+                            exit(-1);\
+                        }\
+                        break;\
+                    case 2:\
+                        buff = (strtok(NULL, " \n"));\
+                        if (labels[(int)*buff-'0'] == -1) {\
+                            printf("No :label %d to jump\n", (int)*buff-'0');\
+                            exit(-1);\
+                        }\
+                        break;\
+                    case 3:\
+                        buff = (strtok(((void*)0), ",\n"));\
+                        if (buff != NULL) {\
+                            if (strncmp(buff,"Rx",1) == 0) {\
+                                fprintf(out, "%s ",++buff);\
+                            }\
+                            buff = (strtok(((void*)0), " \n"));\
+                            if (buff != NULL) {\
+                                fprintf(out, "%s ",++buff);\
+                            } else {\
+                                fprintf(out, "%d ",0);\
+                            }\
+                            break;\
+                        } else {\
+                            printf("Function %d needs an argument\n", cmd_##name);\
+                            exit(-1);\
+                        }\
+                    default:\
+                        buff = (strtok(NULL, " \n"));\
+                        break;\
+                }\
+            }\
+            else 
 #include "cmdlist.h"
-        printf("Error..\n"); //это испрaвит ошибку с последним else
+            {printf("NO SUCH COMMAND\n"); exit(-1);
+            }
 #undef DEF_CMD
+        }
     }
     
     fclose(out);
--- FullStack v2.1/FullStack v2.1/cmdlist.h
+++ FullStack v2.1/FullStack v2.1/cmdlist.h
@@ -1,5 +1,21 @@
-DEF_CMD(WHY,0,{printf("hello");},0)
-DEF_CMD(PUSH,1,{StackPush (&stk, v);},1) //название команды и ее номер
-DEF_CMD(POP,2,{StackPop(&stk);},0)
-DEF_CMD(ADD,3,{StackPush(&stk, StackPop(&stk)+StackPop(&stk));},0)
-DEF_CMD(SUB,4,{StackPush(&stk, -(StackPop(&stk)-StackPop(&stk)));},0)
+DEF_CMD(END,0,{return 0;},none)
+DEF_CMD(PUSH,1,{Stack_push (This, This->cmd[++i]);},num)
+DEF_CMD(POP,2,{Stack_pop(This);},none)
+DEF_CMD(ADD,3,{Stack_push(This, Stack_pop(This)+Stack_pop(This));},none)
+DEF_CMD(SUB,4,{Stack_push(This, -(Stack_pop(This)-Stack_pop(This)));},none)
+DEF_CMD(MUL,5,{Stack_push(This, Stack_pop(This)*Stack_pop(This))},none)
+DEF_CMD(DIV,6, { temp = Stack_pop(This);
+    if (temp != 0) {
+        Stack_push(This, temp);
+        Stack_push(This, 1/(Stack_pop(This)/Stack_pop(This)));
+    } else {
+    printf("Can't divide on zero!");
+    exit(-1);
+    }
+}, none)
+DEF_CMD(MOV, 7, {}, regs)
+DEF_CMD(JMP, 8, {}, label)
+DEF_CMD(JNE, 9, {}, label)
+DEF_CMD(JE, 10, {}, label)
+DEF_CMD(CAL, 11, {}, label)
+DEF_CMD(RET, 12, {}, label)
