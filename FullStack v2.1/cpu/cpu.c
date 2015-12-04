0fbcaf4502fd8e2369bee2ef66b0495af82d66fd
Roman Proskin <opomuc@gmail.com>
2015-12-04 21:16:42 +0300

    Code review 04.12 - CPU

--- FullStack v2.1/cpu/cpu.c
+++ FullStack v2.1/cpu/cpu.c
@@ -17,7 +17,7 @@
 
 typedef int elem_t;
 
-typedef struct stack {
+typedef struct stack { // You have stack structure already!! or you should have
     elem_t* head;
     int count;
 } Stack_t;
@@ -25,8 +25,8 @@ typedef struct stack {
 typedef struct cpu {
     Stack_t stack;
     Stack_t label_stack;
-    unsigned int Rx[REG_NUM];
-    int flag1;
+    unsigned int Rx[REG_NUM]; // just R[__NUM__] is ok. or gprs. in Rx - x means any number
+    int flag1; // flag? flags? why flag1... or cmp_flag if you use it just for comparison
     int* cmd;
     int pc;
     int cmd_num;
@@ -35,13 +35,13 @@ typedef struct cpu {
 
 void CPU_ctor(CPU_t* This) {
     assert(This);
-    This->stack.head = (elem_t*) calloc(STACK_SIZE, sizeof(elem_t));
-    This->label_stack.head = (elem_t*) calloc(LABEL_SIZE, sizeof(elem_t));
-    This->cmd = (int*) calloc(50, sizeof(int));
-    printf("The %d memory has been callocated for stack\n",STACK_SIZE);
-    printf("The %d memory has been callocated for labels\n",LABEL_SIZE);
-    printf("The %d memory has been callocated for commands\n",STACK_SIZE);
-    This->stack.count = 0;
+    This->stack.head = (elem_t*) calloc(STACK_SIZE, sizeof(elem_t)); // STACK_CONSTRUCT
+    This->label_stack.head = (elem_t*) calloc(LABEL_SIZE, sizeof(elem_t)); // STACK_CONSTRUCT
+    This->cmd = (int*) calloc(50, sizeof(int)); // there is no free afterwards
+    printf("The %d memory has been callocated for stack\n",STACK_SIZE); // debug prints below. it is better to place them 
+    printf("The %d memory has been callocated for labels\n",LABEL_SIZE); // right after the allocation takes place.
+    printf("The %d memory has been callocated for commands\n",STACK_SIZE); // not at some random place in the constructor
+    This->stack.count = 0; // STACK_CONSTRUCT
     for (int k = 0; k<=REG_NUM; k++) {
         This->Rx[k] = 0;
     };
@@ -51,11 +51,12 @@ void CPU_ctor(CPU_t* This) {
 }
 
 void CPU_dtor(CPU_t* This) {
-    assert(This);
-    free(This->stack.head);
-    free(This->label_stack.head);
-    printf("\nThe memory has been cleaned\n");
-    exit(0);
+    assert(This); // maybe smth more informative like assert(This && "Can't destruct a NULL pointer")
+    free(This->stack.head); // STACK_DESTRUCT
+    free(This->label_stack.head); // STACK_DESTRUCT
+    printf("\nThe memory has been cleaned\n"); // debug print
+    exit(0); // hey wtf? you don't need to close the program after destruction. what if you have multiple cores??
+    // you forgot about command memory
 }
 
 int cmd_download(CPU_t* This, FILE* in) {
@@ -63,32 +64,32 @@ int cmd_download(CPU_t* This, FILE* in) {
     while (!feof(in)) {
         fscanf(in, "%d", &buff);
         This->cmd[i] = buff;
-        i++;
+        i++;  // do not forget about command memory size.
         count++;
     }
     fclose(in);
     return count;
 }
 
-int Stack_pop(CPU_t* This) {
+int Stack_pop(CPU_t* This) { // Stack_pop from your previous task?
     elem_t buff;
     if (This->stack.count>=1) {
         This->stack.count--;
-        buff=This->stack.head[This->stack.count];
-        This->stack.head[This->stack.count]=0;
+        buff=This->stack.head[This->stack.count]; // return This->stack.head[This->stack.count];
+        This->stack.head[This->stack.count]=0; // that is not cruicial. you can remove it -- see above.
     } else {
-        printf("Pop from empty stack\n");
-        exit(-1);
+        printf("Pop from an empty stack\n");
+        exit(-1); // same here. you dont need to exit completely. OR it should be safe. The best is to call all destructors.
     }
     return buff;
 }
 
 void Stack_push(CPU_t* This, elem_t element) {
-    This->stack.head[This->stack.count] = element;
-    This->stack.count++;
+    This->stack.head[This->stack.count] = element; // why don't you use Stack_pop and Stack_push from 
+    This->stack.count++; // stack overflow?
 }
 
-int Stack_mov(CPU_t* This, unsigned int dest, unsigned int source) {
+int Stack_mov(CPU_t* This, unsigned int dest, unsigned int source) { // this is a CPU command. not Stack
     if (source) {
         This->Rx[dest] = This->Rx[source];
         return 2;
@@ -101,7 +102,7 @@ int Stack_mov(CPU_t* This, unsigned int dest, unsigned int source) {
 void cpu_jmp(CPU_t* This) {
     unsigned int dest = This->cmd[++This->pc];
     This->pc = dest;
-    This->pc--;
+    This->pc--;  // what is this for?
 }
 
 void cpu_cmp(CPU_t* This) {
@@ -112,27 +113,27 @@ void cpu_cmp(CPU_t* This) {
     } else if (a == b) {
         This->flag1 = 0;
     } else This->flag1 = -1;
-    Stack_push(This, b);
-    Stack_push(This, a);
+    Stack_push(This, b); // In my opinion you can think of a specification where you don't need them back
+    Stack_push(This, a); // But if you want to keep those numbers, it is fine
 }
 
 void cpu_jne(CPU_t* This) {
-    cpu_cmp(This);
+    cpu_cmp(This); // this should be a separate instruction, not a function
     if (This->flag1 != 0) {
         cpu_jmp(This);
     }
-    This->pc++;
+    This->pc++; // what is this for?
 }
 
-void cpu_je(CPU_t* This) {
-    cpu_cmp(This);
+void cpu_je(CPU_t* This) { // same as function above
+    cpu_cmp(This);  
     if (This->flag1 == 0) {
         cpu_jmp(This);
     }
     This->pc++;
 }
 
-int ok(CPU_t* This) {
+int ok(CPU_t* This) { // not enough for a CPU checker!..
     if (This->stack.count < 0) {
         printf("counter is below zero");
         exit(-1);
@@ -140,14 +141,14 @@ int ok(CPU_t* This) {
     return 0;
 }
 
-int CPU_dump(CPU_t* This) {
+int CPU_dump(CPU_t* This) { // Dump is not full
     int i;
     printf("\nDUMP IS HERE\n");
-    for (i=0; i < STACK_SIZE; i++) {
+    for (i=0; i < STACK_SIZE; i++) { // print that it is stack and which one! you have two of them
         printf("%d ",This->stack.head[i]);
     }
     printf("\nstack pointer = %d ",This->stack.count);
-    for (i = 1; i <= REG_NUM; i++) {
+    for (i = 1; i <= REG_NUM; i++) { // Why did you left R0 alone? and you definitely don't have R[_REG_NUM_]!!
         printf("\nR%d = %d", i, This->Rx[i]);
     }
     return 1;
@@ -157,7 +158,7 @@ int CPU_run(CPU_t* This) {
     int i = 0;
     int temp = 0;
     for (i=0; ; This->pc++) {
-    switch (This->cmd[This->pc]) {
+    switch (This->cmd[This->pc]) { // indentation
 #define DEF_CMD(name,num,rule,argnum) \
         case num:\
             rule\
@@ -180,6 +181,6 @@ int main() {
     CPU_run(cpu);
     CPU_dump(cpu);
     CPU_dtor(cpu);
-    printf("LOL\n");
+    printf("LOL\n"); // LOL :)
     return 0;
 }
