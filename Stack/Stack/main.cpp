687202fc616bb7d56ad09d21e5da89282d2bca6c
citizenseven <iskhakov@frtk.ru>
2016-03-14 21:21:52 +0300

    Stack update
    Stack review mistakes correction

--- Stack/Stack/main.cpp
+++ Stack/Stack/main.cpp
@@ -22,36 +22,40 @@ public:
     int dump();
 };
 
-CStack::CStack(): data(), size(0) {
+CStack::CStack(): data(),
+size(0) {
     std::cout<<"Stack had been created"<< std::endl;
 }
 
-CStack::CStack(): data(), size(0)  // better to put them on separate lines
-{}
-
 CStack::~CStack() {
-    std::cout<<"Stack has been deleted" <<std::endl; // this is a debug printf... and you forgot spaces around operators
-  // make size = -1 here at least. something to know that stack has been deleted.
+    //std::cout << "Stack has been deleted" << std::endl;
+    size = -1;
 }
 
 int CStack::push(int val) {
-    // where is check?
-    this->data[this->size++] = val;
+    if (size >= 0) {
+        this->data[this->size++] = val;
+    } else {
+        std::cout << "Invalid stack pointer" << std::endl;
+        assert(-1);
+    }
     return 0;
 }
 
 int CStack::pop() {
     if (this->size > 0) {
-        return this->data[this->size--]; // wrong. should be decremented before poping. data[--size] 
-    } else return 0xBAD;  // wrong. you may have 0xbad as a value.
-    // also, remember google code-style I asked you to read?
-    // if you use brackets in if statement, use it in else as well.
+        return this->data[--this->size];
+    } else {
+        std::cout << "Invalid stack pointer" << std::endl;
+        assert(-1);
+    }
+    return 0;
 }
 
 int CStack::dump() {
-    std::cout << "Current stack with size " <<this->size << " is: "; // spaces
-    for (int size_ = 0; size_<size; size_++) {  // either use this-> everywhere, or don't use it at all
-        std::cout <<this->data[size_]<< " ";  // spaces
+    std::cout << "Current stack with size " << this->size << " is: ";
+    for (int size_ = 0; size_<this->size; size_++) {
+        std::cout << this->data[size_] << " ";
     }
     std::cout << std::endl ;
     return 0;
